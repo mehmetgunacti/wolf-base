538a5f2adf382300dd3dd655da1db7de05d7e415
Mehmet Gunacti <mehmet.gunacti@gmail.com>
2023-04-18 04:34:17 +0300

    added 'table names' dropdown to settings page

--- src/modules/settings/pages/settings-page/settings-page.component.ts
+++ src/modules/settings/pages/settings-page/settings-page.component.ts
@@ -1,7 +1,18 @@
 import { Component } from '@angular/core';
+import { WolfBaseTableName } from 'lib';
 
 @Component({
 	selector: 'app-settings-page',
 	templateUrl: './settings-page.component.html'
 })
-export class SettingsPageComponent { }
+export class SettingsPageComponent {
+
+	tableNames: { label: string, value: string }[];
+
+	constructor() {
+
+		this.tableNames = Object.entries(WolfBaseTableName).map(([value, label]) => ({ label, value }));
+
+	}
+
+}
