35a2e9119ad8ef5210e29c7b3b93574b1d49a25d
Mehmet Gunacti <mehmet.gunacti@gmail.com>
2023-04-25 23:32:26 +0300

    added responsive.constant.ts for breakpoints

---
+++ src/lib/constants/responsive.constant.ts
@@ -0,0 +1,10 @@
+
+// IMPORTANT: these have to be the same as node_modules/primeflex/src/_variables.scss
+export enum Breakpoint {
+
+    sm = '576px',
+    md = '768px',
+    lg = '992px',
+    xl = '1200px'
+
+}
