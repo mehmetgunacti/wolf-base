c0d6dc57a7f7324e2f894b4df00578200c49cecf
Mehmet Gunacti <mehmet.gunacti@gmail.com>
2023-07-01 19:28:51 +0300

    removed unused method

--- src/worker/sync.worker.ts
+++ src/worker/sync.worker.ts
@@ -8,14 +8,13 @@
 * Only import pure JavaScript functions or libraries.
 * Also, don't use index.ts files when writing import statments inside the lib folder.
 */
+import { Action, RemoteStorageService } from "lib";
 import { SyncEvent } from "lib/models/sync.model";
 import { localStorageServiceFactory } from "lib/services/localstorage/dexie/factories";
 import { LocalStorageService } from "lib/services/localstorage/local-storage-service.interface";
 import { remoteStorageServiceFactory } from "lib/services/remotestorage/firestore/factories";
-import { sleep } from "lib/utils/helper.tool";
 import { BookmarksSyncAction } from "./actions/bookmarks.action";
 import { PostService } from "./utils";
-import { Action, RemoteStorageService } from "lib";
 
 let isRunning = false;
 
@@ -38,26 +37,6 @@ addEventListener('message', async (a: MessageEvent) => {
 	isRunning = false;
 
 });
-
-async function process(gen: AsyncGenerator<SyncEvent>): Promise<void> {
-
-	try {
-
-		for await (const syncState of gen) {
-
-			await sleep(500);
-			postMessage(syncState);
-
-		}
-
-	} catch (err) {
-
-		console.error(err);
-		postMessage({ message: err });
-
-	}
-
-}
 
 function createActions(): Action<void, Promise<void>>[] {
 
