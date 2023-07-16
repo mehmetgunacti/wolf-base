e8f0a10766eb04c4303ce05e6186c8d87d253a68
Mehmet Gunacti <mehmet.gunacti@gmail.com>
2023-07-16 09:30:04 +0300

    removed obsolete property from bookmark state

--- src/store/bookmark/states/entities.state.ts
+++ src/store/bookmark/states/entities.state.ts
@@ -6,7 +6,6 @@ export interface EntitiesState {
 	entities: Map<UUID, Bookmark>;
 	editDialogVisible: boolean;
 	selected: UUID | null;
-	tagCloudVisible: boolean;
 	clicks: Click[];
 
 }
@@ -16,7 +15,6 @@ export const entitiesInitialState: EntitiesState = {
 	entities: new Map(),
 	editDialogVisible: false,
 	selected: null,
-	tagCloudVisible: false,
 	clicks: []
 
 };
