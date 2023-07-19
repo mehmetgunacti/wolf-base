3e47d3c66d943cf86e9f954b2de12cf8ca2298b8
Mehmet Gunacti <mehmet.gunacti@gmail.com>
2023-07-19 18:37:13 +0300

    removed Firestore configuration from 'sync'

--- src/modules/sync/sync.config.ts
+++ src/modules/sync/sync.config.ts
@@ -1,13 +1,9 @@
-import { FirestoreConfigFormComponent } from "./components/firestore-config-form/firestore-config-form.component";
-import { FirestoreConfigContainerComponent } from "./containers/firestore-config-container/firestore-config-container.component";
 import { SyncSummaryContainerComponent } from "./containers/sync-summary-container/sync-summary-container.component";
 import { SyncPageComponent } from "./pages/sync-page/sync-page.component";
 
 export const components = [
 
 	SyncPageComponent,
-	FirestoreConfigContainerComponent,
-	FirestoreConfigFormComponent,
 	SyncSummaryContainerComponent
 
 ];
