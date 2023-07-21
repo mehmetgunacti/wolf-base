690c055951661aa492286c6d95bb3d5f5bb7fcf4
Mehmet Gunacti <mehmet.gunacti@gmail.com>
2023-07-21 04:53:03 +0300

    removed unused env values

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,13 +1,7 @@
 export const environment = {
 	production: false,
-	firebase: {
-		apiKey: 'AIzaSyBW_cPM4U3AcUtbMDx64KlWNfx703E_NcM',
-		baseURL: 'https://firestore.googleapis.com/v1/',
-		projectId: 'wolf-359-base'
-	},
 	croppie: {
 		styleUrl: 'https://cdnjs.cloudflare.com/ajax/libs/croppie/2.6.5/croppie.min.css',
 		scriptUrl: 'https://cdnjs.cloudflare.com/ajax/libs/croppie/2.6.5/croppie.min.js'
-	},
-	remoteURLLookup: 'https://tools.buzzstream.com/metaDataService?url='
+	}
 };
