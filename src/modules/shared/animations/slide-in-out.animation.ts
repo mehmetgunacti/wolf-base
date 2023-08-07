ff07bf79d52990dbff3bdb879688b8196ed10f84
Mehmet Gunacti <mehmet.gunacti@gmail.com>
2023-08-07 22:48:21 +0300

    formatted code

--- src/modules/shared/animations/slide-in-out.animation.ts
+++ src/modules/shared/animations/slide-in-out.animation.ts
@@ -1,53 +1,7 @@
-// @ANGULAR
-import { animate, keyframes, query, state, style, transition, trigger } from '@angular/animations';
-
-// @WOLF359
-
-// export const slideInOutTrigger = trigger('slideInOut', [
-//     transition('false => true', [
-//         query('false', [
-//             style({ overflow: 'hidden' }),
-//             animate(300, style({
-//                 height: '*',
-//                 opacity: '1'
-//             }))
-//         ], { optional: true })
-//     ]),
-//     transition('true => false', [
-//         query('true', [
-//             style({ overflow: 'visible' }),
-//             animate(300, style({
-//                 height: '0',
-//                 opacity: '0'
-//             }))
-//         ], { optional: true })
-//     ])
-// ]);
-// export const slideInOutTrigger = trigger('slideInOut', [
-//     state('true', style({
-//         // 'max-height': '0',
-//         height: '0',
-//         // opacity: '0',
-//         overflow: 'hidden'
-//     })),
-//     state('false', style({
-//         // 'max-height': '*',
-//         height: '*',
-//         // opacity: '1',
-//         overflow: 'visible'
-//     })),
-//     transition('true => false', [
-//         style({ overflow: 'hidden' }),
-//         animate('300ms linear'),
-//         style({ overflow: 'visible' })
-//     ]),
-//     transition('false => true', [
-//         style({ overflow: 'hidden' }),
-//         animate('300ms linear')
-//     ])
-// ]);
+import { animate, style, transition, trigger } from '@angular/animations';
 
 export const slideUpDownTrigger = trigger('slideUpDown', [
+
 	transition(':enter', [
 
 		// css styles at start of transition
@@ -56,7 +10,6 @@ export const slideUpDownTrigger = trigger('slideUpDown', [
 		// animation and styles at end of transition
 		animate('300ms', style({ height: '*', opacity: 1 }))
 	]),
-
 	transition(':leave', [
 
 		// css styles at start of transition
@@ -65,21 +18,5 @@ export const slideUpDownTrigger = trigger('slideUpDown', [
 		// animation and styles at end of transition
 		animate('300ms', style({ height: 0, opacity: 0 }))
 	])
-]);
 
-// export const slideInOutTrigger = trigger('slideInOut', [
-//     state('true', style({
-//         height: '*',
-//         opacity: 1,
-//     })),
-//     state('false', style({
-//         height: '0',
-//         opacity: 0
-//     })),
-//     transition('true => false', [
-//         animate('0.35s')
-//     ]),
-//     transition('false => true', [
-//         animate('0.35s')
-//     ])
-// ])
+]);
--- src/modules/shared/components/selected-tags/selected-tags.component.ts
+++ src/modules/shared/components/selected-tags/selected-tags.component.ts
@@ -1,19 +1,17 @@
-import { ChangeDetectionStrategy, Component, EventEmitter, Input, OnChanges, Output, SimpleChanges } from '@angular/core';
+import { ChangeDetectionStrategy, Component, EventEmitter, Input, Output } from '@angular/core';
 
 @Component({
 	selector: 'app-selected-tags',
 	templateUrl: './selected-tags.component.html',
 	changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class SelectedTagsComponent implements OnChanges {
+export class SelectedTagsComponent {
 
 	@Input() tags: string[] | undefined | null;
 	@Input() selectedTags: string[] | undefined | null;
 
 	@Output() tagClick: EventEmitter<string> = new EventEmitter();
 	@Output() filterClick: EventEmitter<void> = new EventEmitter();
-
-	ngOnChanges(changes: SimpleChanges): void { }
 
 	onTagClick(name: string): void {
 
@@ -26,98 +24,5 @@ export class SelectedTagsComponent implements OnChanges {
 		this.filterClick.emit();
 
 	}
-
-	// @Input() selectedTags: string[] | undefined | null;
-	// @Input() relatedTags: string[] | undefined | null;
-
-
-	// @Output() resetClick: EventEmitter<string> = new EventEmitter();
-
-	// uiTags!: TagUI[];
-
-	// ngOnChanges(changes: SimpleChanges): void {
-
-	// 	// add fontSize to tags array
-	// 	this.uiTags = this.createUITags();
-
-	// }
-
-
-
-	// onReset(): void {
-
-	// 	this.resetClick.emit();
-
-	// }
-
-	// // calculated font size based on the count property of each tag.
-	// private createUITags(): TagUI[] {
-
-	// 	// Initialize an empty array to hold the ITagUI objects.
-	// 	const uiArr: TagUI[] = [];
-
-	// 	// If the tags array exists.
-	// 	if (this.tags) {
-
-	// 		// Create an array of the count property of each tag.
-	// 		const arr: number[] = this.tags.map((t) => t.count);
-
-	// 		// Initialize a map to hold the font size for each count value.
-	// 		const fontSizeMap: Map<number, string> = new Map();
-
-	// 		// Define an array of font sizes to use.
-	// 		const fontSizeValues = ['0.8em', '1.4em', '1.7em', '2em', '2.3em'];
-
-	// 		// Remove duplicate values and sort the array of counts in ascending order.
-	// 		const uniqueArr = Array.from(new Set(arr));
-	// 		const sortedArr = uniqueArr.sort((a, b) => a - b);
-
-	// 		// Divide the array of counts into 5 buckets of roughly equal size.
-	// 		const bucketSize = Math.ceil(sortedArr.length / 5);
-
-	// 		// For each count value, determine which bucket it falls into and assign the appropriate font size.
-	// 		for (let i = 0; i < sortedArr.length; i++) {
-	// 			const fontSizeIndex = Math.floor(i / bucketSize);
-	// 			const count = sortedArr[i];
-	// 			const fontSize = fontSizeMap.get(count) || fontSizeValues[fontSizeIndex];
-	// 			fontSizeMap.set(count, fontSize);
-	// 		}
-
-	// 		// For each tag, create a new TagUI object with the font size based on the count value, and add it to the uiArr array.
-	// 		uiArr.push(
-	// 			...this.tags.map(
-	// 				(tag) => ({
-	// 					tag,
-	// 					fontSize: fontSizeMap.get(tag.count) || '1em',
-	// 					selectedIdx: this.selectedTags?.indexOf(tag.name) ?? -1,
-	// 					disabled: false,
-	// 					className: this.setClassName(tag.name)
-	// 				})
-	// 			)
-	// 		);
-	// 		uiArr.sort((a, b) => a.tag.name < b.tag.name ? -1 : 1);
-
-	// 	}
-	// 	return uiArr;
-
-	// }
-
-	// private setClassName(name: string): string {
-
-	// 	const selectedTags = this.selectedTags ?? [];
-	// 	if (selectedTags.length === 0)
-	// 		return ''; // do not override css 'color'
-
-	// 	const selected = selectedTags.includes(name);
-	// 	if (selected)
-	// 		return 'selected';
-
-	// 	const relatedTags = this.relatedTags ?? [];
-	// 	if (relatedTags.includes(name))
-	// 		return 'related';
-
-	// 	return 'disabled';
-
-	// }
 
 }
