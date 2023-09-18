3c21097986b7bd327d41c52d5f40942801c2ca00
Mehmet Gunacti <mehmet.gunacti@gmail.com>
2023-09-18 03:31:43 +0300

    added knowledge base tables

--- src/lib/constants/database.constant.ts
+++ src/lib/constants/database.constant.ts
@@ -5,7 +5,19 @@ export enum WolfBaseTableName {
 	bookmarks_remote = 'bookmarks_remote',
 	bookmarks_trash = 'bookmarks_trash',
 	bookmarks_clicks = 'bookmarks_clicks',
+
+	kb_entries = 'kb_entries',
+	kb_entries_sync = 'kb_entries_sync',
+	kb_entries_remote = 'kb_entries_remote',
+	kb_entries_trash = 'kb_entries_trash',
+	
+	kb_contents = 'kb_contents',
+	kb_contents_sync = 'kb_contents_sync',
+	kb_contents_remote = 'kb_contents_remote',
+	kb_contents_trash = 'kb_contents_trash',
+
 	configuration = 'configuration',
+
 	logs = 'logs'
 
 }
--- src/services/dexie/wolfbase.database.ts
+++ src/services/dexie/wolfbase.database.ts
@@ -1,7 +1,7 @@
 import Dexie from 'dexie';
 import { UUID, UUID_ISODate } from 'lib/constants/common.constant';
 import { CONF_KEYS, WolfBaseTableName } from 'lib/constants/database.constant';
-import { LogMessage, RemoteMetadata, SyncData } from 'lib/models';
+import { KBContent, KBEntry, LogMessage, RemoteMetadata, SyncData } from 'lib/models';
 import { Bookmark, Click } from 'lib/models/bookmark.model';
 import { DexieConfiguration } from 'lib/models/database.model';
 
@@ -20,11 +20,23 @@ export const wolfBaseDBFactory = (): WolfBaseDB => {
 		tables: {
 
 			// bookmarks
-			bookmarks: 'id, *tags, clicks',
+			bookmarks: 'id',
 			bookmarks_sync: 'id',
 			bookmarks_trash: '++, id, name',
-			bookmarks_clicks: 'id, current',
 			bookmarks_remote: 'id',
+			bookmarks_clicks: 'id',
+
+			// knowledge base entry
+			kb_entries: 'id',
+			kb_entries_sync: 'id',
+			kb_entries_trash: '++, id, name',
+			kb_entries_remote: 'id',
+
+			// knowledge base content
+			kb_contents: 'id',
+			kb_contents_sync: 'id',
+			kb_contents_trash: '++, id, name',
+			kb_contents_remote: 'id',
 
 			// configuration
 			configuration: '',
@@ -33,18 +45,32 @@ export const wolfBaseDBFactory = (): WolfBaseDB => {
 			logs: '++id, category, entityId'
 
 		},
-		version: 15
+		version: 16
 	});
 
 };
 
 export class WolfBaseDB extends Dexie {
 
+	// bookmarks
 	bookmarks: Dexie.Table<Bookmark, UUID>;
 	bookmarks_sync: Dexie.Table<SyncData, UUID>;
 	bookmarks_remote: Dexie.Table<RemoteMetadata, UUID>;
 	bookmarks_trash: Dexie.Table<Bookmark, number>;
-	clicks: Dexie.Table<Click, UUID>;
+	bookmarks_clicks: Dexie.Table<Click, UUID>;
+
+	// knowledge base entries
+	kb_entries: Dexie.Table<KBEntry, UUID>;
+	kb_entries_sync: Dexie.Table<SyncData, UUID>;
+	kb_entries_remote: Dexie.Table<RemoteMetadata, UUID>;
+	kb_entries_trash: Dexie.Table<KBEntry, number>;
+
+	// knowledge base contents
+	kb_contents: Dexie.Table<KBContent, UUID>;
+	kb_contents_sync: Dexie.Table<SyncData, UUID>;
+	kb_contents_remote: Dexie.Table<RemoteMetadata, UUID>;
+	kb_contents_trash: Dexie.Table<KBContent, number>;
+
 	configuration: Dexie.Table<string | boolean, CONF_KEYS>;
 	logs: Dexie.Table<LogMessage, number>;
 
@@ -58,8 +84,20 @@ export class WolfBaseDB extends Dexie {
 		this.bookmarks_sync = this.table(WolfBaseTableName.bookmarks_sync);
 		this.bookmarks_remote = this.table(WolfBaseTableName.bookmarks_remote);
 		this.bookmarks_trash = this.table(WolfBaseTableName.bookmarks_trash);
-		this.clicks = this.table(WolfBaseTableName.bookmarks_clicks);
+		this.bookmarks_clicks = this.table(WolfBaseTableName.bookmarks_clicks);
+
+		this.kb_entries = this.table(WolfBaseTableName.kb_entries);
+		this.kb_entries_sync = this.table(WolfBaseTableName.kb_entries_sync);
+		this.kb_entries_remote = this.table(WolfBaseTableName.kb_entries_remote);
+		this.kb_entries_trash = this.table(WolfBaseTableName.kb_entries_trash);
+
+		this.kb_contents = this.table(WolfBaseTableName.kb_contents);
+		this.kb_contents_sync = this.table(WolfBaseTableName.kb_contents_sync);
+		this.kb_contents_remote = this.table(WolfBaseTableName.kb_contents_remote);
+		this.kb_contents_trash = this.table(WolfBaseTableName.kb_contents_trash);
+
 		this.configuration = this.table(WolfBaseTableName.configuration);
+
 		this.logs = this.table(WolfBaseTableName.logs);
 
 		this.on('populate', () => {
