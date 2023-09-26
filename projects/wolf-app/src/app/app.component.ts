ef7a62f209f1a656a12273ba1fcad6c5141be9c6
Mehmet Gunacti <mehmet.gunacti@gmail.com>
2023-09-26 23:40:03 +0300

    add manifest.webmanifest to assets in angular.json

--- projects/wolf-app/src/app/app.component.ts
+++ projects/wolf-app/src/app/app.component.ts
@@ -1,44 +1,9 @@
 import { Component } from '@angular/core';
-import { Store } from '@ngrx/store';
-// import { fadeOutTrigger } from 'modules/shared';
-import { Observable, delay, of, switchMap, takeWhile, tap } from 'rxjs';
-import { selCoreIsInitialized } from 'store/selectors/core-configuration.selectors';
 
 @Component({
 	selector: 'app-root',
 	template: `
-		<ng-container *ngIf="initialized$ | async; else loading">
-			<router-outlet></router-outlet>
-		</ng-container>
-		<ng-template #loading>
-			<div id="splashScreen" @fadeOut>
-				<img src="assets/logo.svg" alt="Wolf Base loading...">
-			</div>
-		</ng-template>
-	`,
-	animations: [] // fadeOutTrigger]
+		<router-outlet></router-outlet>
+	`
 })
-export class AppComponent {
-
-	initialized$: Observable<boolean>;
-	
-	constructor(store: Store) {
-
-		this.initialized$ = store.select(selCoreIsInitialized).pipe(
-
-			switchMap((value: boolean) => {
-
-				// to tweak splashscreen duration:
-				// delay(), _splashscreen.scss and fade-out.animation.ts
-				if (value)
-					return of(true).pipe(delay(1400));
-				return of(false);
-
-			}),
-			takeWhile(value => !value, true)
-
-		);
-
-	}
-
-}
+export class AppComponent { }
