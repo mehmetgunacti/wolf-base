cb787a9e090b93d00b9c7cedd3eba96bf7d0e3e0
Mehmet Gunacti <mehmet.gunacti@gmail.com>
2023-12-10 23:26:22 +0300

    fixed firebase pr build command

--- projects/wolf-app/src/version.ts
+++ projects/wolf-app/src/version.ts
@@ -1,6 +1,6 @@
 // this file is automatically created
 export const buildInfo = {
-	version: '0.0.33',
-	builtTime: '2023-12-10T20:21:44.483Z',
-	builtNumber: '506'
+	version: '0.0.34',
+	builtTime: '2023-12-10T20:26:28.457Z',
+	builtNumber: '507'
 };
