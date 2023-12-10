cd382e11d36f0192cf04cc52c31eb71714063087
Mehmet Gunacti <mehmet.gunacti@gmail.com>
2023-12-11 00:38:20 +0300

    fixed firebase.json "public" value

--- projects/wolf-app/src/version.ts
+++ projects/wolf-app/src/version.ts
@@ -1,6 +1,6 @@
 // this file is automatically created
 export const buildInfo = {
-	version: '0.0.38',
-	builtTime: '2023-12-10T21:31:37.367Z',
-	builtNumber: '511'
+	version: '0.0.39',
+	builtTime: '2023-12-10T21:38:24.113Z',
+	builtNumber: '512'
 };
