31ba8aa7ae15a88b29d502d4473d5a11a2cffc93
Mehmet Gunacti <mehmet.gunacti@gmail.com>
2023-12-11 00:31:34 +0300

    fix "public" -> "entryPoint"

--- projects/wolf-app/src/version.ts
+++ projects/wolf-app/src/version.ts
@@ -1,6 +1,6 @@
 // this file is automatically created
 export const buildInfo = {
-	version: '0.0.37',
-	builtTime: '2023-12-10T21:26:49.262Z',
-	builtNumber: '510'
+	version: '0.0.38',
+	builtTime: '2023-12-10T21:31:37.367Z',
+	builtNumber: '511'
 };
