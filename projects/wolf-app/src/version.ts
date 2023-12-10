f782084229e07139d983b23ab268d66f60c52b81
Mehmet Gunacti <mehmet.gunacti@gmail.com>
2023-12-10 23:33:13 +0300

    fix upgrade node version to 18

--- projects/wolf-app/src/version.ts
+++ projects/wolf-app/src/version.ts
@@ -1,6 +1,6 @@
 // this file is automatically created
 export const buildInfo = {
-	version: '0.0.34',
-	builtTime: '2023-12-10T20:26:28.457Z',
-	builtNumber: '507'
+	version: '0.0.35',
+	builtTime: '2023-12-10T20:33:17.082Z',
+	builtNumber: '508'
 };
