58e3d11fa5b3058f6f2c7649c9ba55ceb37e6db5
Mehmet Gunacti <mehmet.gunacti@gmail.com>
2023-12-10 23:16:17 +0300

    increased version

--- projects/wolf-app/src/version.ts
+++ projects/wolf-app/src/version.ts
@@ -1,6 +1,6 @@
 // this file is automatically created
 export const buildInfo = {
-	version: '0.0.1',
-	builtTime: '2023-12-10T20:14:08.142Z',
-	builtNumber: '505'
+	version: '0.0.33',
+	builtTime: '2023-12-10T20:21:44.483Z',
+	builtNumber: '506'
 };
