4d3eeca6193d176bf6bb17909e714dda9c72bd71
Mehmet Gunacti <mehmet.gunacti@gmail.com>
2023-12-15 13:22:47 +0300

    syncing of notes

--- projects/wolf-app/src/modules/cloud/containers/cloud-summary-container/cloud-summary-container.component.ts
+++ projects/wolf-app/src/modules/cloud/containers/cloud-summary-container/cloud-summary-container.component.ts
@@ -1,13 +1,15 @@
 import { ChangeDetectionStrategy, Component, inject } from '@angular/core';
 import { Action, Store } from '@ngrx/store';
-import { CloudTask, SyncTaskType } from 'lib';
-import { Observable } from 'rxjs';
+import { CloudTask, SyncTaskType, WolfEntity } from 'lib';
+import { Observable, combineLatest, map, toArray } from 'rxjs';
 import * as bmActions from 'store/actions/bookmark.actions';
+import * as noteActions from 'store/actions/note.actions';
 import { selBookmarkCloudTasks } from 'store/selectors/bookmark-selectors/bookmark-cloud.selectors';
+import { selNoteCloudTasks } from 'store/selectors/note-selectors/note-cloud.selectors';
 
-function getAction(task: CloudTask): Action | null {
+function getBookmarkAction(taskType: SyncTaskType): Action | null {
 
-	switch (task.type) {
+	switch (taskType) {
 
 		case SyncTaskType.local_new:
 			return bmActions.syncLocalNew();
@@ -43,6 +45,56 @@ function getAction(task: CloudTask): Action | null {
 
 }
 
+function getNoteAction(taskType: SyncTaskType): Action | null {
+
+	switch (taskType) {
+
+		case SyncTaskType.local_new:
+			return noteActions.syncLocalNew();
+
+		case SyncTaskType.local_updated:
+			return noteActions.syncLocalUpdated();
+
+		case SyncTaskType.local_deleted:
+			return noteActions.syncLocalDeleted();
+
+		case SyncTaskType.remote_new:
+			return noteActions.syncRemoteNew();
+
+		case SyncTaskType.remote_updated:
+			return noteActions.syncRemoteUpdated();
+
+		case SyncTaskType.remote_deleted:
+			return noteActions.syncRemoteDeleted();
+
+		case SyncTaskType.clicked:
+			return noteActions.uploadClicked();
+
+		case SyncTaskType.deleted_deleted:
+			return noteActions.syncDeletedDeleted();
+
+		// case CloudTaskType.updated_updated:
+		// case CloudTaskType.updated_deleted:
+		// case CloudTaskType.deleted_updated:
+		// 	return EMPTY;
+
+	}
+	return null;
+
+}
+
+function getAction(task: CloudTask): Action | null {
+
+	if (WolfEntity.bookmark.name === task.entity.name)
+		return getBookmarkAction(task.type);
+
+	if (WolfEntity.note.name === task.entity.name)
+		return getNoteAction(task.type);
+
+	return null;
+
+}
+
 @Component({
 	selector: 'app-cloud-summary-container',
 	templateUrl: './cloud-summary-container.component.html',
@@ -57,7 +109,12 @@ export class CloudSummaryContainerComponent {
 
 	constructor() {
 
-		this.tasks$ = this.store.select(selBookmarkCloudTasks);
+		this.tasks$ = combineLatest([
+			this.store.select(selBookmarkCloudTasks),
+			this.store.select(selNoteCloudTasks)
+		]).pipe(
+			map(([bookmarks, notes]) => [...bookmarks, ...notes])
+		);
 
 	}
 
--- projects/wolf-app/src/version.ts
+++ projects/wolf-app/src/version.ts
@@ -1,6 +1,6 @@
 // this file is automatically created
 export const buildInfo = {
-	version: '0.0.50',
-	builtTime: '2023-12-15T09:54:48.343Z',
-	builtNumber: '542'
+	version: '0.0.51',
+	builtTime: '2023-12-15T10:24:34.973Z',
+	builtNumber: '543'
 };
