9994137d00b9d3b203a82d55ee985c38c80052bb
Mehmet Gunacti <mehmet.gunacti@gmail.com>
2023-12-19 06:50:50 +0300

    fixed timePast pipe

--- projects/wolf-lib/src/lib/pipes/time-past.pipe.ts
+++ projects/wolf-lib/src/lib/pipes/time-past.pipe.ts
@@ -13,26 +13,26 @@ export class TimePastPipe implements PipeTransform {
 			return 'just now';
 
 		if (diffInSeconds <= 90)
-			return `${diffInSeconds} seconds ago`;
+			return `${diffInSeconds} second${diffInSeconds > 1 ? 's' : ''} ago`;
 
 		const diffInMinutes = Math.floor(diffInSeconds / 60);
 		if (diffInMinutes <= 90)
-			return `${diffInMinutes} minutes ago`;
+			return `${diffInMinutes} minute${diffInMinutes > 1 ? 's' : ''} ago`;
 
 		const diffInHours = Math.floor(diffInMinutes / 60);
 		if (diffInHours < 24)
-			return `${diffInHours} hours ago`;
+			return `${diffInHours} hour${diffInHours > 1 ? 's' : ''} ago`;
 
 		const diffInDays = Math.floor(diffInHours / 24);
 		if (diffInDays < 30)
-			return `${diffInDays} days ago`;
+			return `${diffInDays} day${diffInDays > 1 ? 's' : ''} ago`;
 
 		const diffInMonths = Math.floor(diffInDays / 30);
 		if (diffInMonths < 12)
-			return `${diffInMonths} months ago`;
+			return `${diffInMonths} month${diffInMonths > 1 ? 's' : ''} ago`;
 
 		const diffInYears = Math.floor(diffInMonths / 12);
-		return `${diffInYears} years ago`;
+		return `${diffInYears} year${diffInYears > 1 ? 's' : ''} ago`;
 
 	}
 
