2c82c5a9889424417e4df0b4403ccc2befe01f27
Mehmet Gunacti <mehmet.gunacti@gmail.com>
2023-12-23 19:28:55 +0300

    adjusted cloud no taks message

--- projects/wolf-app/src/version.ts
+++ projects/wolf-app/src/version.ts
@@ -1,6 +1,6 @@
 // this file is automatically created
 export const buildInfo = {
-	version: '0.0.60',
-	builtTime: '2023-12-23T16:00:06.311Z',
-	builtNumber: '587'
+	version: '0.0.61',
+	builtTime: '2023-12-23T16:29:03.025Z',
+	builtNumber: '591'
 };
