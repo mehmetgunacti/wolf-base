c95c91a04006444d0a856cadec87ecb6e7a9ed4f
Mehmet Gunacti <mehmet.gunacti@gmail.com>
2023-12-28 17:59:40 +0300

    refactoring of scss files, css classes

--- projects/wolf-app/src/version.ts
+++ projects/wolf-app/src/version.ts
@@ -1,6 +1,6 @@
 // this file is automatically created
 export const buildInfo = {
-	version: '0.0.65',
-	builtTime: '2023-12-27T04:03:50.120Z',
-	builtNumber: '607'
+	version: '0.0.66',
+	builtTime: '2023-12-28T14:59:47.981Z',
+	builtNumber: '609'
 };
