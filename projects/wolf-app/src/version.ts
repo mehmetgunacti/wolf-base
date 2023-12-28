e1caf8689965a24d2737e8a17522a338d2fc7cc6
Mehmet Gunacti <mehmet.gunacti@gmail.com>
2023-12-28 19:32:03 +0300

    added p css style

--- projects/wolf-app/src/version.ts
+++ projects/wolf-app/src/version.ts
@@ -1,6 +1,6 @@
 // this file is automatically created
 export const buildInfo = {
-	version: '0.0.66',
-	builtTime: '2023-12-28T14:59:47.981Z',
-	builtNumber: '609'
+	version: '0.0.67',
+	builtTime: '2023-12-28T16:32:12.008Z',
+	builtNumber: '611'
 };
