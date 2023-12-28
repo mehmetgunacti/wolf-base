3823e64b1723149843338353339462d001d12938
Mehmet Gunacti <mehmet.gunacti@gmail.com>
2023-12-28 20:52:00 +0300

    added gap to card class

--- projects/wolf-app/src/version.ts
+++ projects/wolf-app/src/version.ts
@@ -1,6 +1,6 @@
 // this file is automatically created
 export const buildInfo = {
-	version: '0.0.67',
-	builtTime: '2023-12-28T16:32:12.008Z',
-	builtNumber: '611'
+	version: '0.0.68',
+	builtTime: '2023-12-28T17:52:04.966Z',
+	builtNumber: '612'
 };
