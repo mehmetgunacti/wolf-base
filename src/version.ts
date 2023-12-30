41bd28d11fc5aef8c4b05ca8a0b4b2b85ba074e9
Mehmet Gunacti <mehmet.gunacti@gmail.com>
2023-12-30 05:52:56 +0300

    fixed github deplyment error

--- src/version.ts
+++ src/version.ts
@@ -1,6 +1,6 @@
 // this file is automatically created
 export const buildInfo = {
-	version: '0.0.1',
-	builtTime: '2023-12-30T02:45:43.687Z',
-	builtNumber: '619'
+	version: '0.1.1',
+	builtTime: '2023-12-30T02:53:00.561Z',
+	builtNumber: '620'
 };
