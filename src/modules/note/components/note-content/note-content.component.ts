570d8b33d12b4cf559ed1378ffe62125f9591d89
Mehmet Gunacti <mehmet.gunacti@gmail.com>
2023-12-30 08:19:42 +0300

    added highlight.js

--- src/modules/note/components/note-content/note-content.component.ts
+++ src/modules/note/components/note-content/note-content.component.ts
@@ -1,12 +1,13 @@
 import { DOCUMENT } from '@angular/common';
 import { ChangeDetectionStrategy, Component, Input, inject } from '@angular/core';
 import { NoteContent } from 'lib/models';
-import { BehaviorSubject, Observable, Subject, combineLatest, map } from 'rxjs';
+import { BehaviorSubject, Observable, Subject, combineLatest, concat, concatMap, from, map } from 'rxjs';
 import { DOMService } from 'services';
 
 declare global {
 	interface Window {
 		markdownit: any;
+		hljs: any;
 	}
 }
 
@@ -40,10 +41,28 @@ export class NoteContentComponent {
 			if (!content || !loaded)
 				return null;
 
+			const hljs = this.document.defaultView?.hljs;
+
 			const config = {
+
 				html: true,
 				breaks: true,
-				linkify: true
+				linkify: true,
+				typographer: true,
+				highlight: function (str: string, lang: string) {
+
+					if (lang && hljs.getLanguage(lang)) {
+						try {
+							return hljs.highlight(str, { language: lang }).value;
+						} catch (e) {
+							console.error(e);
+							throw new Error('highlight-js related error occured!');
+						}
+					}
+					return ''; // use external default escaping
+
+				}
+
 			};
 			const md = this.document.defaultView?.markdownit(config);
 			return md.render(content);
@@ -54,18 +73,40 @@ export class NoteContentComponent {
 
 	constructor() {
 
-		this.domService
-			.appendScriptToBody('https://cdn.jsdelivr.net/npm/markdown-it@14.0.0/dist/markdown-it.min.js')
-			.subscribe({
-				complete: () => {
+		const styleUrls = [
+			'https://cdnjs.cloudflare.com/ajax/libs/highlight.js/11.9.0/styles/default.min.css',
+		];
+		const scriptUrls = [
+			'https://cdnjs.cloudflare.com/ajax/libs/highlight.js/11.9.0/highlight.min.js',
+			'https://cdnjs.cloudflare.com/ajax/libs/highlight.js/11.9.0/languages/java.min.js',
+			'https://cdnjs.cloudflare.com/ajax/libs/highlight.js/11.9.0/languages/javascript.min.js',
+			'https://cdnjs.cloudflare.com/ajax/libs/highlight.js/11.9.0/languages/typescript.min.js',
+			'https://cdnjs.cloudflare.com/ajax/libs/highlight.js/11.9.0/languages/sql.min.js',
+			'https://cdn.jsdelivr.net/npm/markdown-it@14.0.0/dist/markdown-it.min.js'
+		]
 
-					const md = this.document.defaultView?.markdownit();
-					if (!md)
-						throw new Error('markdown-it not in window scope!');
-					this.subjectMD.next(true);
+		concat(
 
-				}
-			});
+			from(styleUrls).pipe(
+				concatMap(url => this.domService.appendLinkToHead(url))
+			),
+			from(scriptUrls).pipe(
+				concatMap(url => this.domService.appendScriptToBody(url))
+			)
+
+		).subscribe({
+
+			complete: () => {
+
+				const hljs = this.document.defaultView?.hljs;
+				const md = this.document.defaultView?.markdownit();
+				if (!md)
+					throw new Error('markdown-it not in window scope!');
+				this.subjectMD.next(true);
+
+			}
+
+		});
 
 	}
 
