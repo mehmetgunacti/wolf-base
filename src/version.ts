c54523b71ba9607695d760ff09e52f832a7aecf8
Mehmet Gunacti <mehmet.gunacti@gmail.com>
2023-12-31 23:26:03 +0300

    fix for table overflow-x

--- src/version.ts
+++ src/version.ts
@@ -1,6 +1,6 @@
 // this file is automatically created
 export const buildInfo = {
-	version: '0.1.4',
-	builtTime: '2023-12-31T20:13:52.221Z',
-	builtNumber: '627'
+	version: '0.1.5',
+	builtTime: '2023-12-31T20:26:35.466Z',
+	builtNumber: '628'
 };
