2d8ac9fabb9c1fa5e631223c23957f1d8d63ce44
Mehmet Gunacti <mehmet.gunacti@gmail.com>
2023-12-31 23:13:40 +0300

    fixed bookmark form bg color

--- src/version.ts
+++ src/version.ts
@@ -1,6 +1,6 @@
 // this file is automatically created
 export const buildInfo = {
-	version: '0.1.3',
-	builtTime: '2023-12-31T17:18:10.901Z',
-	builtNumber: '624'
+	version: '0.1.4',
+	builtTime: '2023-12-31T20:13:52.221Z',
+	builtNumber: '627'
 };
