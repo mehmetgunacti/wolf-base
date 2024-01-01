00993458226553d6661c5dee1172c17d0d083ab0
Mehmet Gunacti <mehmet.gunacti@gmail.com>
2024-01-01 16:23:41 +0300

    refactored markdown-it, created markdown service

--- src/modules/note/components/note-content/note-content.component.ts
+++ src/modules/note/components/note-content/note-content.component.ts
@@ -1,29 +1,19 @@
-import { DOCUMENT } from '@angular/common';
 import { ChangeDetectionStrategy, Component, Input, inject } from '@angular/core';
 import { NoteContent } from 'lib/models';
-import { BehaviorSubject, Observable, Subject, combineLatest, concat, concatMap, from, map } from 'rxjs';
-import { DOMService } from 'services';
-
-declare global {
-	interface Window {
-		markdownit: any;
-		hljs: any;
-	}
-}
+import { Observable, Subject, map } from 'rxjs';
+import { MarkdownService } from 'services/markdown.service';
 
 @Component({
 	selector: 'app-note-content',
 	templateUrl: './note-content.component.html',
-	styleUrls: ['./note-content.component.scss'],
+	styleUrl: './note-content.component.scss',
 	changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class NoteContentComponent {
 
 	private subjectContent: Subject<string | null> = new Subject<string | null>();
-	private subjectMD: BehaviorSubject<boolean> = new BehaviorSubject<boolean>(false);
 
-	private domService: DOMService = inject(DOMService);
-	private readonly document: Document = inject(DOCUMENT);
+	private markdownService: MarkdownService = inject(MarkdownService);
 
 	@Input() set content(nc: NoteContent | null) {
 
@@ -31,83 +21,10 @@ export class NoteContentComponent {
 
 	}
 
-	result$: Observable<string | null> = combineLatest([
-		this.subjectContent.asObservable(),
-		this.subjectMD.asObservable()
-	]).pipe(
+	result$: Observable<string | null> = this.subjectContent.asObservable().pipe(
 
-		map(([content, loaded]) => {
-
-			if (!content || !loaded)
-				return null;
-
-			const hljs = this.document.defaultView?.hljs;
-
-			const config = {
-
-				html: true,
-				breaks: true,
-				linkify: true,
-				typographer: true,
-				highlight: function (str: string, lang: string) {
-
-					if (lang && hljs.getLanguage(lang)) {
-						try {
-							return hljs.highlight(str, { language: lang }).value;
-						} catch (e) {
-							console.error(e);
-							throw new Error('highlight-js related error occured!');
-						}
-					}
-					return ''; // use external default escaping
-
-				}
-
-			};
-			const md = this.document.defaultView?.markdownit(config);
-			return md.render(content);
-
-		})
+		map(content => this.markdownService.render(content))
 
 	);
-
-	constructor() {
-
-		const styleUrls = [
-			'https://cdnjs.cloudflare.com/ajax/libs/highlight.js/11.9.0/styles/default.min.css',
-		];
-		const scriptUrls = [
-			'https://cdnjs.cloudflare.com/ajax/libs/highlight.js/11.9.0/highlight.min.js',
-			'https://cdnjs.cloudflare.com/ajax/libs/highlight.js/11.9.0/languages/java.min.js',
-			'https://cdnjs.cloudflare.com/ajax/libs/highlight.js/11.9.0/languages/javascript.min.js',
-			'https://cdnjs.cloudflare.com/ajax/libs/highlight.js/11.9.0/languages/typescript.min.js',
-			'https://cdnjs.cloudflare.com/ajax/libs/highlight.js/11.9.0/languages/sql.min.js',
-			'https://cdn.jsdelivr.net/npm/markdown-it@14.0.0/dist/markdown-it.min.js'
-		]
-
-		concat(
-
-			from(styleUrls).pipe(
-				concatMap(url => this.domService.appendLinkToHead(url))
-			),
-			from(scriptUrls).pipe(
-				concatMap(url => this.domService.appendScriptToBody(url))
-			)
-
-		).subscribe({
-
-			complete: () => {
-
-				const hljs = this.document.defaultView?.hljs;
-				const md = this.document.defaultView?.markdownit();
-				if (!md)
-					throw new Error('markdown-it not in window scope!');
-				this.subjectMD.next(true);
-
-			}
-
-		});
-
-	}
 
 }
---
+++ src/services/markdown.service.ts
@@ -0,0 +1,97 @@
+import { Injectable } from '@angular/core';
+
+import hljs from 'highlight.js/lib/core';
+import asciidoc from 'highlight.js/lib/languages/asciidoc.js';
+import autohotkey from 'highlight.js/lib/languages/autohotkey.js';
+import bash from 'highlight.js/lib/languages/bash.js';
+import c from 'highlight.js/lib/languages/c.js';
+import cpp from 'highlight.js/lib/languages/cpp.js';
+import csharp from 'highlight.js/lib/languages/csharp.js';
+import css from 'highlight.js/lib/languages/css.js';
+import dockerfile from 'highlight.js/lib/languages/dockerfile.js';
+import gradle from 'highlight.js/lib/languages/gradle.js';
+import http from 'highlight.js/lib/languages/http.js';
+import ini from 'highlight.js/lib/languages/ini.js';
+import java from 'highlight.js/lib/languages/java.js';
+import javascript from 'highlight.js/lib/languages/javascript.js';
+import json from 'highlight.js/lib/languages/json.js';
+import kotlin from 'highlight.js/lib/languages/kotlin.js';
+import markdown from 'highlight.js/lib/languages/markdown.js';
+import powershell from 'highlight.js/lib/languages/powershell.js';
+import properties from 'highlight.js/lib/languages/properties.js';
+import python from 'highlight.js/lib/languages/python.js';
+import scss from 'highlight.js/lib/languages/scss.js';
+import shell from 'highlight.js/lib/languages/shell.js';
+import sql from 'highlight.js/lib/languages/sql.js';
+import typescript from 'highlight.js/lib/languages/typescript.js';
+import xml from 'highlight.js/lib/languages/xml.js';
+import yaml from 'highlight.js/lib/languages/yaml.js';
+import { default as MarkdownIt, default as markdownit } from 'markdown-it';
+
+@Injectable({ providedIn: 'root' })
+export class MarkdownService {
+
+	private md: MarkdownIt;
+
+	constructor() {
+
+		hljs.registerLanguage('asciidoc', asciidoc);
+		hljs.registerLanguage('autohotkey', autohotkey);
+		hljs.registerLanguage('bash', bash);
+		hljs.registerLanguage('c', c);
+		hljs.registerLanguage('cpp', cpp);
+		hljs.registerLanguage('csharp', csharp);
+		hljs.registerLanguage('css', css);
+		hljs.registerLanguage('dockerfile', dockerfile);
+		hljs.registerLanguage('gradle', gradle);
+		hljs.registerLanguage('http', http);
+		hljs.registerLanguage('ini', ini);
+		hljs.registerLanguage('java', java);
+		hljs.registerLanguage('javascript', javascript);
+		hljs.registerLanguage('json', json);
+		hljs.registerLanguage('kotlin', kotlin);
+		hljs.registerLanguage('markdown', markdown);
+		hljs.registerLanguage('powershell', powershell);
+		hljs.registerLanguage('properties', properties);
+		hljs.registerLanguage('python', python);
+		hljs.registerLanguage('scss', scss);
+		hljs.registerLanguage('shell', shell);
+		hljs.registerLanguage('sql', sql);
+		hljs.registerLanguage('typescript', typescript);
+		hljs.registerLanguage('xml', xml);
+		hljs.registerLanguage('yaml', yaml);
+
+		const config = {
+
+			html: true,
+			breaks: true,
+			linkify: true,
+			typographer: true,
+			highlight: function (str: string, language: string) {
+
+				if (language && hljs.getLanguage(language)) {
+					try {
+						return hljs.highlight(str, { language }).value;
+					} catch (e) {
+						console.error(e);
+						throw new Error('highlight-js related error occured!');
+					}
+				}
+				return ''; // use external default escaping
+
+			}
+
+		};
+		this.md = markdownit(config);
+
+	}
+
+	render(content: string | null): string | null {
+
+		if (!content)
+			return null;
+		return this.md.render(content);
+
+	}
+
+}
