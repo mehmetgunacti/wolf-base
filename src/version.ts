9bd15cf5987f5ad1dc678128b6470534c9aeb33d
Mehmet Gunacti <mehmet.gunacti@gmail.com>
2024-01-01 17:25:31 +0300

    added primary color for md checkbox styling

--- src/version.ts
+++ src/version.ts
@@ -1,6 +1,6 @@
 // this file is automatically created
 export const buildInfo = {
-	version: '0.1.7',
-	builtTime: '2024-01-01T14:09:15.298Z',
-	builtNumber: '632'
+	version: '0.1.8',
+	builtTime: '2024-01-01T14:25:57.481Z',
+	builtNumber: '634'
 };
