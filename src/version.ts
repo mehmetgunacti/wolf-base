f4a58e7d564fc2e232954e3dad647daf37b7533d
Mehmet Gunacti <mehmet.gunacti@gmail.com>
2024-01-03 22:21:00 +0300

    refactored styling of list items (notes)

--- src/version.ts
+++ src/version.ts
@@ -1,6 +1,6 @@
 // this file is automatically created
 export const buildInfo = {
-	version: '0.1.12',
-	builtTime: '2024-01-03T11:57:15.026Z',
-	builtNumber: '654'
+	version: '0.1.13',
+	builtTime: '2024-01-03T19:21:07.124Z',
+	builtNumber: '659'
 };
