a583bfffe1f6f65b36f31dcf5d2e81a2d227a521
Mehmet Gunacti <mehmet.gunacti@gmail.com>
2024-01-03 14:18:58 +0300

    styling of notes, general css classes added

--- src/version.ts
+++ src/version.ts
@@ -1,6 +1,6 @@
 // this file is automatically created
 export const buildInfo = {
-	version: '0.1.10',
-	builtTime: '2024-01-02T19:48:46.667Z',
-	builtNumber: '650'
+	version: '0.1.11',
+	builtTime: '2024-01-03T11:19:05.247Z',
+	builtNumber: '652'
 };
