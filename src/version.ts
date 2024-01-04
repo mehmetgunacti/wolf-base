f5163a420abb7191cef8cfce3f0bd77c10077240
Mehmet Gunacti <mehmet.gunacti@gmail.com>
2024-01-04 21:02:11 +0300

    fixed webmanifest logo

--- src/version.ts
+++ src/version.ts
@@ -1,6 +1,6 @@
 // this file is automatically created
 export const buildInfo = {
-	version: '0.1.15',
-	builtTime: '2024-01-04T17:52:07.351Z',
-	builtNumber: '664'
+	version: '0.1.16',
+	builtTime: '2024-01-04T18:02:33.952Z',
+	builtNumber: '665'
 };
