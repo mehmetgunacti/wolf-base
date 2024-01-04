119248c82866d62b69cc5ae3c9da9c8db5324847
Mehmet Gunacti <mehmet.gunacti@gmail.com>
2024-01-04 23:39:11 +0300

    update styling of ul, ol

--- src/version.ts
+++ src/version.ts
@@ -1,6 +1,6 @@
 // this file is automatically created
 export const buildInfo = {
-	version: '0.1.19',
-	builtTime: '2024-01-04T20:33:16.590Z',
-	builtNumber: '670'
+	version: '0.1.20',
+	builtTime: '2024-01-04T20:39:17.797Z',
+	builtNumber: '671'
 };
