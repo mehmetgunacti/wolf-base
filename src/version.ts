78a0e4e5bf9ace57325bc15b5fab9cff2459d3c0
Mehmet Gunacti <mehmet.gunacti@gmail.com>
2024-01-04 21:10:33 +0300

    fixed pre code line break

--- src/version.ts
+++ src/version.ts
@@ -1,6 +1,6 @@
 // this file is automatically created
 export const buildInfo = {
-	version: '0.1.16',
-	builtTime: '2024-01-04T18:02:33.952Z',
-	builtNumber: '665'
+	version: '0.1.17',
+	builtTime: '2024-01-04T18:10:36.571Z',
+	builtNumber: '666'
 };
