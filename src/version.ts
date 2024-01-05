7571edf1123c79cfeec8d3c5d9a503cd243dbb9c
Mehmet Gunacti <mehmet.gunacti@gmail.com>
2024-01-06 01:53:20 +0300

    adjusted md checkbox styling

--- src/version.ts
+++ src/version.ts
@@ -1,6 +1,6 @@
 // this file is automatically created
 export const buildInfo = {
-	version: '0.1.22',
-	builtTime: '2024-01-05T21:23:00.503Z',
-	builtNumber: '676'
+	version: '0.1.23',
+	builtTime: '2024-01-05T22:53:29.533Z',
+	builtNumber: '678'
 };
