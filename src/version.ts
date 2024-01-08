ca424c31375a122a851fbfc0ba0eac377ceb3895
Mehmet Gunacti <mehmet.gunacti@gmail.com>
2024-01-08 16:06:45 +0300

    adjusted note styling

--- src/version.ts
+++ src/version.ts
@@ -1,6 +1,6 @@
 // this file is automatically created
 export const buildInfo = {
-	version: '0.1.27',
-	builtTime: '2024-01-07T07:41:10.056Z',
-	builtNumber: '686'
+	version: '0.1.28',
+	builtTime: '2024-01-08T13:07:10.698Z',
+	builtNumber: '697'
 };
