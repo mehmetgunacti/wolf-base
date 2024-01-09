6a104e97a9a45fc172428c3ce199f023aa2e5d21
Mehmet Gunacti <mehmet.gunacti@gmail.com>
2024-01-10 00:06:55 +0300

    fixed button styling

--- src/version.ts
+++ src/version.ts
@@ -1,6 +1,6 @@
 // this file is automatically created
 export const buildInfo = {
-	version: '0.1.32',
-	builtTime: '2024-01-09T20:56:52.720Z',
-	builtNumber: '710'
+	version: '0.1.33',
+	builtTime: '2024-01-09T21:07:01.359Z',
+	builtNumber: '711'
 };
