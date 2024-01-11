b9132df64e4d527bcec3ca7b8dbc5abb31177e42
Mehmet Gunacti <mehmet.gunacti@gmail.com>
2024-01-11 06:45:00 +0300

    fixed nav link animation

--- src/version.ts
+++ src/version.ts
@@ -1,6 +1,6 @@
 // this file is automatically created
 export const buildInfo = {
-	version: '0.1.34',
-	builtTime: '2024-01-11T03:05:40.118Z',
-	builtNumber: '715'
+	version: '0.1.35',
+	builtTime: '2024-01-11T03:58:13.855Z',
+	builtNumber: '716'
 };
