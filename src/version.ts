48120e3cd6bb12173d7db19c1e0889cca325c48e
Mehmet Gunacti <mehmet.gunacti@gmail.com>
2024-01-11 04:50:11 +0300

    refactored color values and styling

--- src/version.ts
+++ src/version.ts
@@ -1,6 +1,6 @@
 // this file is automatically created
 export const buildInfo = {
-	version: '0.1.33',
-	builtTime: '2024-01-09T21:07:01.359Z',
-	builtNumber: '711'
+	version: '0.1.34',
+	builtTime: '2024-01-11T03:05:40.118Z',
+	builtNumber: '715'
 };
