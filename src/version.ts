7e08211fadb6af9ec3632453dd08d4d982ece0a2
Mehmet Gunacti <mehmet.gunacti@gmail.com>
2024-01-17 00:34:06 +0300

    fixed blockquote listing of editor

--- src/version.ts
+++ src/version.ts
@@ -1,6 +1,6 @@
 // this file is automatically created
 export const buildInfo = {
-	version: '0.1.43',
-	builtTime: '2024-01-16T21:16:26.638Z',
-	builtNumber: '749'
+	version: '0.1.44',
+	builtTime: '2024-01-16T21:37:40.872Z',
+	builtNumber: '751'
 };
