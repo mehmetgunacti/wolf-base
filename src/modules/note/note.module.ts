2543f7f6ad289ffb898c7361002e942a4fe4a1b4
Mehmet Gunacti <mehmet.gunacti@gmail.com>
2024-01-17 08:58:16 +0300

    fixed cyclic dependency

--- src/modules/note/note.module.ts
+++ src/modules/note/note.module.ts
@@ -1,7 +1,8 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { ReactiveFormsModule } from '@angular/forms';
-import { CroppieModule, EditorModule, MarkdownModule } from '@lib';
+import { CroppieModule, MarkdownModule } from '@lib';
+import { EditorModule } from 'lib/components/editor/editor.module';
 import { SharedModule } from 'modules/shared/shared.module';
 import { NoteRoutingModule } from './note-routing.module';
 import * as fromConfig from './note.config';
--- src/version.ts
+++ src/version.ts
@@ -1,6 +1,6 @@
 // this file is automatically created
 export const buildInfo = {
-	version: '0.1.46',
-	builtTime: '2024-01-17T05:45:54.638Z',
-	builtNumber: '760'
+	version: '0.1.47',
+	builtTime: '2024-01-17T05:58:44.573Z',
+	builtNumber: '761'
 };
