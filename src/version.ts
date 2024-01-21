92d74e17760677fff04ab6317283ecc71f537906
Mehmet Gunacti <mehmet.gunacti@gmail.com>
2024-01-21 18:31:55 +0300

    hide scrollbar on small screen

--- src/version.ts
+++ src/version.ts
@@ -1,6 +1,6 @@
 // this file is automatically created
 export const buildInfo = {
-	version: '0.1.53',
-	builtTime: '2024-01-21T15:07:52.611Z',
-	builtNumber: '778'
+	version: '0.1.54',
+	builtTime: '2024-01-21T15:32:00.847Z',
+	builtNumber: '779'
 };
