9640c18cff56d7c3cc3fbcf36f790f726d766c6c
Mehmet Gunacti <mehmet.gunacti@gmail.com>
2024-01-21 18:07:46 +0300

    webmanifest color adjustment

--- src/version.ts
+++ src/version.ts
@@ -1,6 +1,6 @@
 // this file is automatically created
 export const buildInfo = {
-	version: '0.1.52',
-	builtTime: '2024-01-21T14:41:54.491Z',
-	builtNumber: '776'
+	version: '0.1.53',
+	builtTime: '2024-01-21T15:07:52.611Z',
+	builtNumber: '778'
 };
