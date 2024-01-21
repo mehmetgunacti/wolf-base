3f425bdf1890fd9e70d6200ef1020bb134f60786
Mehmet Gunacti <mehmet.gunacti@gmail.com>
2024-01-21 17:35:53 +0300

    fixed note parent links

--- src/version.ts
+++ src/version.ts
@@ -1,6 +1,6 @@
 // this file is automatically created
 export const buildInfo = {
-	version: '0.1.51',
-	builtTime: '2024-01-21T00:42:29.766Z',
-	builtNumber: '774'
+	version: '0.1.52',
+	builtTime: '2024-01-21T14:41:54.491Z',
+	builtNumber: '776'
 };
