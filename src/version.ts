93ab499c2c13e7339190d947e6720f0ee16a48e0
Mehmet Gunacti <mehmet.gunacti@gmail.com>
2024-01-27 02:18:12 +0300

    fixed outline styling

--- src/version.ts
+++ src/version.ts
@@ -1,6 +1,6 @@
 // this file is automatically created
 export const buildInfo = {
-	version: '0.1.63',
-	builtTime: '2024-01-26T21:01:01.784Z',
-	builtNumber: '810'
+	version: '0.1.64',
+	builtTime: '2024-01-26T23:31:55.896Z',
+	builtNumber: '814'
 };
