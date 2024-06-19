e5f1a29a2578e57a2f01ed022f42e4aef54bf442
Mehmet Gunacti <mehmet.gunacti@gmail.com>
2024-06-19 08:33:43 +0300

    added toDateObject function to utils

--- src/lib/utils/date-time.util.ts
+++ src/lib/utils/date-time.util.ts
@@ -7,3 +7,12 @@ export function compareISODateStrings(d1: ISODateString, d2: ISODateString): num
 	return date1.getTime() - date2.getTime();
 
 }
+
+export function toDateObject(millisecondsSinceEpoch: string): Date | null {
+
+	const ms = parseInt(millisecondsSinceEpoch, 10);
+	if (isNaN(ms))
+		return null;
+	return new Date(ms); // Create a date object from milliseconds
+
+}
