c3f067abc69c70b087b5d09b88cd5d4030493a72
Mehmet Gunacti <mehmet.gunacti@gmail.com>
2024-06-29 07:38:30 +0300

    fixed width of top info bar of database page

--- src/version.ts
+++ src/version.ts
@@ -1,6 +1,6 @@
 // this file is automatically created
 export const buildInfo = {
-	version: '0.1.115',
-	builtTime: '2024-06-29T04:27:17.816Z',
-	builtNumber: '918'
+	version: '0.1.116',
+	builtTime: '2024-06-29T04:38:53.386Z',
+	builtNumber: '919'
 };
