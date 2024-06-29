6b4949642ff1ee226b3100f5b49b3ff8f930a837
Mehmet Gunacti <mehmet.gunacti@gmail.com>
2024-06-29 07:26:47 +0300

    css styling ng-progress

--- src/version.ts
+++ src/version.ts
@@ -1,6 +1,6 @@
 // this file is automatically created
 export const buildInfo = {
-	version: '0.1.114',
-	builtTime: '2024-06-29T01:19:01.114Z',
-	builtNumber: '907'
+	version: '0.1.115',
+	builtTime: '2024-06-29T04:27:17.816Z',
+	builtNumber: '918'
 };
