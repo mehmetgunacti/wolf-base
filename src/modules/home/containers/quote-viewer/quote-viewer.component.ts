aff29bac1d0675d588b881b3d7b3a18c498dbf25
Mehmet Gunacti <mehmet.gunacti@gmail.com>
2024-06-30 07:55:59 +0300

    fix default quote (picard)

--- src/modules/home/containers/quote-viewer/quote-viewer.component.ts
+++ src/modules/home/containers/quote-viewer/quote-viewer.component.ts
@@ -11,7 +11,7 @@ const picard: Quote = {
 	id: 'dummyId',
 	name: 'Seize the time… Live now. Make now always the most precious time. Now will never come again.',
 	author: 'Capt. Jean-Luc Picard',
-	context: 'Star Trek: The Next Generation - The Inner Light'
+	context: 'Star Trek: The Next Generation - ‘The Inner Light’'
 
 };
 
