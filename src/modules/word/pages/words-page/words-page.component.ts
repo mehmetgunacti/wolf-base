c74e532b98bd5cbcf06b820e652a1e830d63da22
Mehmet Gunacti <mehmet.gunacti@gmail.com>
2024-07-01 04:56:27 +0300

    remove temporary fix words page

--- src/modules/word/pages/words-page/words-page.component.ts
+++ src/modules/word/pages/words-page/words-page.component.ts
@@ -1,7 +1,4 @@
-import { ChangeDetectionStrategy, Component, inject } from '@angular/core';
-import { LocalRepositoryService, Word } from '@lib';
-import { LOCAL_REPOSITORY_SERVICE } from 'app/app.config';
-import { v4 as uuidv4 } from 'uuid';
+import { ChangeDetectionStrategy, Component } from '@angular/core';
 
 @Component({
 	selector: 'app-words-page',
@@ -9,26 +6,4 @@ import { v4 as uuidv4 } from 'uuid';
 	styleUrls: ['./words-page.component.scss'],
 	changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class WordsPageComponent {
-
-	private local: LocalRepositoryService = inject(LOCAL_REPOSITORY_SERVICE);
-
-
-	async fix(): Promise<void> {
-
-		const list: Word[] = await this.local.words.list();
-		console.log(list);
-		list.forEach(w => w.definitions.forEach(d => d.id = uuidv4()));
-
-		list.forEach(async (w) => {
-
-			await this.local.words.update(w.id, w);
-
-		})
-
-		console.log(list);
-
-
-	}
-
-}
+export class WordsPageComponent { }
--- src/version.ts
+++ src/version.ts
@@ -1,6 +1,6 @@
 // this file is automatically created
 export const buildInfo = {
-	version: '0.1.120',
-	builtTime: '2024-07-01T01:55:25.486Z',
-	builtNumber: '947'
+	version: '0.1.121',
+	builtTime: '2024-07-01T02:05:48.777Z',
+	builtNumber: '948'
 };
