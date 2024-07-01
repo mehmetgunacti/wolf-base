c086069a8892bb4e0ff393c50ff39e45362f2dc8
Mehmet Gunacti <mehmet.gunacti@gmail.com>
2024-07-01 04:08:38 +0300

    make switch component 'control' input optional

--- src/lib/components/switch/switch.component.ts
+++ src/lib/components/switch/switch.component.ts
@@ -1,13 +1,48 @@
-import { Component, InputSignal, input } from '@angular/core';
+import { Component, DestroyRef, EventEmitter, InputSignal, OnInit, Output, effect, inject, input } from '@angular/core';
+import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
 import { FormControl } from '@angular/forms';
+
+export const fcBoolean = () => new FormControl<boolean>(false, { nonNullable: true });
 
 @Component({
 	selector: 'w-switch',
 	templateUrl: './switch.component.html',
 	styleUrl: './switch.component.scss'
 })
-export class SwitchComponent {
+export class SwitchComponent implements OnInit {
 
-	control: InputSignal<FormControl<boolean>> = input.required();
+	control = input(fcBoolean(), { transform: (fc: FormControl<boolean> | null | undefined): FormControl<boolean> => fc ?? fcBoolean() });
+	checked = input(false);
+
+	destroyRef = inject(DestroyRef);
+
+	@Output() on: EventEmitter<boolean> = new EventEmitter();
+	@Output() off: EventEmitter<boolean> = new EventEmitter();
+
+	constructor() {
+
+		effect(() => {
+
+			const checked = this.checked();
+			this.control().setValue(checked);
+
+		});
+
+	}
+
+	ngOnInit(): void {
+
+		this.control().valueChanges.pipe(
+			takeUntilDestroyed(this.destroyRef)
+		).subscribe(val => {
+
+			if (val)
+				this.on.emit();
+			else
+				this.off.emit();
+
+		});
+
+	}
 
 }
