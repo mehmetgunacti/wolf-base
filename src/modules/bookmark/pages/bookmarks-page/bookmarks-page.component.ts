9c7e0236d1a1839b3f8d2220456a13a5e3ee5d9d
Mehmet Gunacti <mehmet.gunacti@gmail.com>
2024-07-14 11:50:25 +0300

    remove redundant code

--- src/modules/bookmark/pages/bookmarks-page/bookmarks-page.component.ts
+++ src/modules/bookmark/pages/bookmarks-page/bookmarks-page.component.ts
@@ -3,7 +3,6 @@ import { Store } from '@ngrx/store';
 import { Observable } from 'rxjs';
 import { fromClipboard, openAddBookmarkDialog } from 'store/actions/bookmark.actions';
 import { selBookmarkShaking } from 'store/selectors/bookmark-selectors/bookmark-ui.selectors';
-import { selCore_isBigScreen } from 'store/selectors/core-ui.selectors';
 
 @Component({
 	selector: 'app-bookmarks-page',
@@ -13,14 +12,12 @@ import { selCore_isBigScreen } from 'store/selectors/core-ui.selectors';
 })
 export class BookmarksPageComponent {
 
-	isBigScreen$: Observable<boolean>;
 	isShaking$: Observable<boolean>;
 
 	private store: Store = inject(Store);
 
 	constructor() {
 
-		this.isBigScreen$ = this.store.select(selCore_isBigScreen);
 		this.isShaking$ = this.store.select(selBookmarkShaking);
 
 	}
