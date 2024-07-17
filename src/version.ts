1c8da797802bc79585724a21d07becbc37afd0e9
Mehmet Gunacti <mehmet.gunacti@gmail.com>
2024-07-18 00:00:17 +0300

    update styling for various elements

--- src/version.ts
+++ src/version.ts
@@ -1,6 +1,6 @@
 // this file is automatically created
 export const buildInfo = {
-	version: '0.1.140',
-	builtTime: '2024-07-17T20:45:47.513Z',
-	builtNumber: '997'
+	version: '0.1.141',
+	builtTime: '2024-07-17T21:00:26.140Z',
+	builtNumber: '998'
 };
