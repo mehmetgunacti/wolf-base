8173e75ed4b206b04850d525c0aa00a45fca11f0
Mehmet Gunacti <mehmet.gunacti@gmail.com>
2024-07-21 02:36:29 +0300

    adjust badge alignment

--- src/version.ts
+++ src/version.ts
@@ -1,6 +1,6 @@
 // this file is automatically created
 export const buildInfo = {
-	version: '0.1.147',
-	builtTime: '2024-07-20T23:17:27.354Z',
-	builtNumber: '1018'
+	version: '0.1.148',
+	builtTime: '2024-07-20T23:36:32.528Z',
+	builtNumber: '1019'
 };
