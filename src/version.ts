21ad39d7ad941babba0f430131b150fd25a883f8
Mehmet Gunacti <mehmet.gunacti@gmail.com>
2024-07-20 17:29:38 +0300

    change quiz choices visibility animation

--- src/version.ts
+++ src/version.ts
@@ -1,6 +1,6 @@
 // this file is automatically created
 export const buildInfo = {
-	version: '0.1.142',
-	builtTime: '2024-07-20T14:17:27.101Z',
-	builtNumber: '1009'
+	version: '0.1.143',
+	builtTime: '2024-07-20T14:30:09.710Z',
+	builtNumber: '1010'
 };
