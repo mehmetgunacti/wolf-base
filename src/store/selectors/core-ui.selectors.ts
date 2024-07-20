d2287d0921bc9ea6185a6e5be8e577bb57bbbc6b
Mehmet Gunacti <mehmet.gunacti@gmail.com>
2024-07-21 00:08:00 +0300

    add selector for 'now'

--- src/store/selectors/core-ui.selectors.ts
+++ src/store/selectors/core-ui.selectors.ts
@@ -29,3 +29,10 @@ export const selCore_theme = createSelector(
 	(state: CoreUIState) => state.theme
 
 );
+
+export const selCore_now = createSelector(
+
+	selCore_uiState,
+	(state: CoreUIState) => state.now
+
+);
