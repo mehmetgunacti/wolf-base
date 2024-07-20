274d9e3e420c694cd5369d61962c8195dea719bf
Mehmet Gunacti <mehmet.gunacti@gmail.com>
2024-07-21 02:17:22 +0300

    add missing scss import

--- src/version.ts
+++ src/version.ts
@@ -1,6 +1,6 @@
 // this file is automatically created
 export const buildInfo = {
-	version: '0.1.146',
-	builtTime: '2024-07-20T22:29:59.511Z',
-	builtNumber: '1016'
+	version: '0.1.147',
+	builtTime: '2024-07-20T23:17:27.354Z',
+	builtNumber: '1018'
 };
