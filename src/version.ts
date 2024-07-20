00255b706becb49f3e46ef42c9a3404d13f4c33a
Mehmet Gunacti <mehmet.gunacti@gmail.com>
2024-07-20 18:18:15 +0300

    make choices overflow hidden

--- src/version.ts
+++ src/version.ts
@@ -1,6 +1,6 @@
 // this file is automatically created
 export const buildInfo = {
-	version: '0.1.144',
-	builtTime: '2024-07-20T15:13:41.825Z',
-	builtNumber: '1011'
+	version: '0.1.145',
+	builtTime: '2024-07-20T15:18:19.388Z',
+	builtNumber: '1012'
 };
