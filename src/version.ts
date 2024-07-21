7f00bb381d6ca09d0bfc470f995b53344339ff89
Mehmet Gunacti <mehmet.gunacti@gmail.com>
2024-07-21 08:27:12 +0300

    replace quote <div> with <pre>

--- src/version.ts
+++ src/version.ts
@@ -1,6 +1,6 @@
 // this file is automatically created
 export const buildInfo = {
-	version: '0.1.151',
-	builtTime: '2024-07-21T03:39:15.042Z',
-	builtNumber: '1022'
+	version: '0.1.152',
+	builtTime: '2024-07-21T05:27:33.297Z',
+	builtNumber: '1024'
 };
