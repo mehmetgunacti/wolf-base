f6eae440afaa189dbe409570ba2028ab62c7ec2d
Mehmet Gunacti <mehmet.gunacti@gmail.com>
2024-07-23 04:37:48 +0300

    fix blink color

--- src/version.ts
+++ src/version.ts
@@ -1,6 +1,6 @@
 // this file is automatically created
 export const buildInfo = {
-	version: '0.1.155',
-	builtTime: '2024-07-22T19:18:31.038Z',
-	builtNumber: '1027'
+	version: '0.1.156',
+	builtTime: '2024-07-23T16:45:14.616Z',
+	builtNumber: '1029'
 };
