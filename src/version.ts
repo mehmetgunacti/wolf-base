f74e7df35ef07448dcf6e0c402ba123d41f75563
Mehmet Gunacti <mehmet.gunacti@gmail.com>
2024-08-09 17:08:41 +0300

    increase quote height

--- src/version.ts
+++ src/version.ts
@@ -1,6 +1,6 @@
 // this file is automatically created
 export const buildInfo = {
-	version: '0.1.165',
-	builtTime: '2024-08-09T13:53:02.691Z',
-	builtNumber: '1049'
+	version: '0.1.166',
+	builtTime: '2024-08-09T14:08:45.695Z',
+	builtNumber: '1050'
 };
