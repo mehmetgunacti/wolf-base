317deb9f6181d5e6baf43ac35b2f7eb4d93532d5
Mehmet Gunacti <mehmet.gunacti@gmail.com>
2024-08-13 15:24:24 +0300

    add slideDown to task

--- src/modules/project/components/project/project.component.ts
+++ src/modules/project/components/project/project.component.ts
@@ -1,4 +1,4 @@
-import { ChangeDetectionStrategy, Component, input, InputSignal, output } from '@angular/core';
+import { ChangeDetectionStrategy, Component, input, InputSignal, output, signal } from '@angular/core';
 import { Project, ProjectStatusLabels, slideDownTrigger, Task, TaskPriorityLabels, UUID } from '@lib';
 
 @Component({
@@ -13,16 +13,26 @@ export class ProjectComponent {
 	protected ProjectStatusLabels = ProjectStatusLabels;
 	protected TaskPriorityLabels = TaskPriorityLabels;
 
+	protected tasksExpanded = signal<Record<UUID, boolean>>({});
+
+	// @Input
 	project: InputSignal<Project> = input.required();
 	infoVisible: InputSignal<boolean> = input(false);
 
+	// @Output
 	toggleInfo = output<boolean>();
 	newTask = output<void>();
 	viewTask = output<UUID>();
 
-	onToggle(): void {
+	onToggleInfo(): void {
 
 		this.toggleInfo.emit(!this.infoVisible());
+
+	}
+
+	onToggleTask(id: UUID): void {
+
+		this.tasksExpanded.update(map => { map[id] = !map[id]; return map; });
 
 	}
 
