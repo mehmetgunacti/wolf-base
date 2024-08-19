e657010e14cf1f63c1692f08cc4b932c35b63f6d
Mehmet Gunacti <mehmet.gunacti@gmail.com>
2024-08-19 03:56:30 +0300

    sort tasks

--- src/version.ts
+++ src/version.ts
@@ -1,6 +1,6 @@
 // this file is automatically created
 export const buildInfo = {
-	version: '0.1.178',
-	builtTime: '2024-08-17T17:49:55.764Z',
-	builtNumber: '1083'
+	version: '0.1.179',
+	builtTime: '2024-08-19T00:56:38.187Z',
+	builtNumber: '1086'
 };
