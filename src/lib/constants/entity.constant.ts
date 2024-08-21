759c2c133144f8a8d7197168b2eba64b9200a422
Mehmet Gunacti <mehmet.gunacti@gmail.com>
2024-08-21 15:41:03 +0300

    add label to EntityName

--- src/lib/constants/entity.constant.ts
+++ src/lib/constants/entity.constant.ts
@@ -2,7 +2,9 @@ export class EntityName {
 
 	constructor(
 		public name: string,
-		public plural: string
+		public plural: string,
+		public label: string,
+		public labelPlural: string
 	) { }
 
 	toString(): string {
@@ -15,13 +17,13 @@ export class EntityName {
 
 export class WolfEntity {
 
-	static bookmark = new EntityName('bookmark', 'bookmarks');
-	static note = new EntityName('note', 'notes');
-	static note_content = new EntityName('note_content', 'note_content');
-	static project = new EntityName('project', 'projects');
-	static quizEntry = new EntityName('quiz_entry', 'quiz_entries');
-	static quote = new EntityName('quote', 'quotes');
-	static task = new EntityName('task', 'tasks');
-	static word = new EntityName('word', 'words');
+	static bookmark = new EntityName('bookmark', 'bookmarks', 'Bookmark', 'Bookmarks');
+	static note = new EntityName('note', 'notes', 'Note', 'Notes');
+	static note_content = new EntityName('note_content', 'note_content', 'Note Content', 'Note Content');
+	static project = new EntityName('project', 'projects', 'Project', 'Projects');
+	static quizEntry = new EntityName('quiz_entry', 'quiz_entries', 'Quiz Entry', 'Quiz Entries');
+	static quote = new EntityName('quote', 'quotes', 'Quote', 'Quotes');
+	static task = new EntityName('task', 'tasks', 'Task', 'Tasks');
+	static word = new EntityName('word', 'words', 'Word', 'Words');
 
 }
