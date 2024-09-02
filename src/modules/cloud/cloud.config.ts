a913656dd55352034d28aa43e419ece09e2ad3e2
Mehmet Gunacti <mehmet.gunacti@gmail.com>
2024-09-02 08:50:28 +0300

    remove unused component

--- src/modules/cloud/cloud.config.ts
+++ src/modules/cloud/cloud.config.ts
@@ -1,6 +1,5 @@
 import { CloudConflictComponent } from "./components/cloud-conflict/cloud-conflict.component";
 import { CloudTaskComponent } from "./components/cloud-task/cloud-task.component";
-import { CloudConflictContainerComponent } from "./containers/cloud-conflict-container/cloud-conflict-container.component";
 import { CloudSummaryContainerComponent } from "./containers/cloud-summary-container/cloud-summary-container.component";
 import { CloudPageComponent } from "./pages/cloud-page/cloud-page.component";
 
@@ -9,7 +8,6 @@ export const components = [
 	CloudPageComponent,
 	CloudSummaryContainerComponent,
 	CloudTaskComponent,
-	CloudConflictComponent,
-	CloudConflictContainerComponent
+	CloudConflictComponent
 
 ];
