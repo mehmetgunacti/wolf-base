677484a59faaf2e591e54e42d97c89a732cda874
Mehmet Gunacti <mehmet.gunacti@gmail.com>
2024-09-02 17:49:56 +0300

    navigate after bookmark update

--- src/store/effects/bookmark-effects/bookmark-entity-update.effects.ts
+++ src/store/effects/bookmark-effects/bookmark-entity-update.effects.ts
@@ -2,6 +2,7 @@ import { inject, Injectable } from '@angular/core';
 import { AppEntities, AppEntityType, isEntityOfType } from '@lib';
 import { Actions, createEffect, ofType } from '@ngrx/effects';
 import { filter, map } from 'rxjs/operators';
+import { navigate } from 'store/actions/core-navigation.actions';
 import { showNotification } from 'store/actions/core-notification.actions';
 import * as entityActions from 'store/actions/entity.actions';
 
@@ -17,6 +18,18 @@ export class BookmarkEntityUpdateEffects {
 			ofType(entityActions.updateSuccess),
 			filter(isEntityOfType(AppEntityType.bookmark)),
 			map(({ entityType }) => showNotification({ severity: 'success', detail: `${AppEntities[entityType].label} updated` }))
+
+		)
+
+	);
+
+	navigate$ = createEffect(
+
+		() => this.actions$.pipe(
+
+			ofType(entityActions.updateSuccess),
+			filter(isEntityOfType(AppEntityType.bookmark)),
+			map(({ id, entityType }) => navigate({ url: [`/${AppEntities[entityType].plural}`], queryParams: { id } }))
 
 		)
 
