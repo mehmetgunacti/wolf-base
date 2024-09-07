d21e960fa4229d9c60288ed57e992b621ebe97c4
Mehmet Gunacti <mehmet.gunacti@gmail.com>
2024-09-07 08:07:49 +0300

    remove redundant code

--- src/modules/database/pages/database-page/database-page.component.ts
+++ src/modules/database/pages/database-page/database-page.component.ts
@@ -1,60 +1,15 @@
-import { Component, OnDestroy, inject } from '@angular/core';
-import { FormControl } from '@angular/forms';
-import { LocalRepositoryNames } from '@lib';
+import { Component, inject } from '@angular/core';
 import { Store } from '@ngrx/store';
-import { Subscription, startWith, tap } from 'rxjs';
-import { backupDatabase, loadValues, search } from 'store/actions/database.actions';
+import { backupDatabase } from 'store/actions/database.actions';
 
 @Component({
 	selector: 'app-database-page',
 	templateUrl: './database-page.component.html',
 	styleUrls: ['./database-page.component.scss']
 })
-export class DatabasePageComponent implements OnDestroy {
-
-	tableNames: { label: string, value?: string, disabled?: boolean }[];
-	fcTableName = new FormControl();
-	fcSearch = new FormControl('');
-
-	subscriptions: Subscription = new Subscription();
+export class DatabasePageComponent {
 
 	private store: Store = inject(Store);
-
-	constructor() {
-
-		this.tableNames = [
-			{ label: 'Select...', disabled: true },
-			...Object.entries(LocalRepositoryNames).map(([value, label]) => ({ label, value }))
-		];
-
-		this.subscriptions.add(
-
-			this.fcTableName.valueChanges.pipe(
-
-				tap(tablename => this.store.dispatch(loadValues({ tablename })))
-
-			).subscribe()
-
-		);
-
-		this.subscriptions.add(
-
-			this.fcSearch.valueChanges.pipe(
-
-				startWith(null),
-				tap(searchValue => this.store.dispatch(search({ value: searchValue })))
-
-			).subscribe()
-
-		);
-
-	}
-
-	ngOnDestroy(): void {
-
-		this.subscriptions.unsubscribe();
-
-	}
 
 	onBackupDatabase(): void {
 
