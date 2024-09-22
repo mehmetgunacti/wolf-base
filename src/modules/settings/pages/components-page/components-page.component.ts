91def348086f82124d6b814a52dd19f873b462e7
Mehmet Gunacti <mehmet.gunacti@gmail.com>
2024-09-22 06:46:36 +0300

    add component page animation

--- src/modules/settings/pages/components-page/components-page.component.ts
+++ src/modules/settings/pages/components-page/components-page.component.ts
@@ -1,13 +1,16 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component, signal } from '@angular/core';
+import { slideUpDownTrigger } from '@lib';
 
 @Component({
 	selector: 'app-components-page',
 	templateUrl: './components-page.component.html',
 	styleUrls: ['./components-page.component.scss'],
+	animations: [slideUpDownTrigger],
 	changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class ComponentsPageComponent {
 
-
+	alertsVisible = signal<boolean>(true);
+	buttonsVisible = signal<boolean>(false);
 
 }
