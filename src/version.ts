eeb9144d0ac36952d3f9d8c77585e8c8ea66aa8a
Mehmet Gunacti <mehmet.gunacti@gmail.com>
2024-10-09 23:37:26 +0300

    fix exceeding styles

--- src/version.ts
+++ src/version.ts
@@ -1,6 +1,6 @@
 // this file is automatically created
 export const buildInfo = {
-	version: '0.1.191',
-	builtTime: '2024-10-09T20:31:09.327Z',
-	builtNumber: '1343'
+	version: '0.1.192',
+	builtTime: '2024-10-09T20:37:30.339Z',
+	builtNumber: '1344'
 };
