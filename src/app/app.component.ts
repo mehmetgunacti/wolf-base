223fe97fcbba266a05efaf4a8c6905f7cbff6dbb
Mehmet Gunacti <mehmet.gunacti@gmail.com>
2024-10-12 02:50:03 +0300

    add main components

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,27 +1,33 @@
-import { Component } from "@angular/core";
-import { RouterOutlet } from '@angular/router';
+import { Component, computed, inject, signal } from "@angular/core";
+import { Store } from '@ngrx/store';
+import { delay, of } from 'rxjs';
+import { environment } from '../environments/environment';
+import { SidebarState } from '../lib/constants';
+import { selCore_sidebarState } from '../store/selectors';
+import { appImports } from './app.config';
 
 @Component({
 	selector: 'app-root',
 	standalone: true,
-	imports: [ RouterOutlet ],
+	imports: appImports,
 	templateUrl: './app.component.html',
 	styleUrl: './app.component.scss',
 })
 export class AppComponent {
 
-	title = "wolf-base";
+	splashVisible = signal(true);
+	sidebarState = inject(Store).selectSignal(selCore_sidebarState);
+	sidebarOpen = computed(() => this.sidebarState() === SidebarState.FULL);
+	sidebarBigFull = computed(() => this.sidebarState() === SidebarState.BIG_FULL);
+	sidebarBigHalf = computed(() => this.sidebarState() === SidebarState.BIG_HALF);
 
 	constructor() {
 
-		const a = this.abc("");
-
-	}
-
-	abc(a: string): string {
-
-		console.log();
-		return '';
+		// to tweak splashscreen animation & duration:
+		// delay(), splashscreen-component.scss and splash-screen.animation.ts
+		of(false)
+			.pipe(delay(environment.splash)) // splash screen visible for n ms
+			.subscribe(() => this.splashVisible.set(false));
 
 	}
 
--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,7 +1,20 @@
 import { ApplicationConfig, provideZoneChangeDetection, isDevMode } from '@angular/core'
-import { provideRouter } from '@angular/router'
+import { provideRouter, RouterOutlet } from '@angular/router'
 import { routes } from './app.routes'
 import { provideServiceWorker } from '@angular/service-worker'
+import { HeaderComponent } from './header/header.component';
+import { NavComponent } from './nav/nav.component';
+import { NavOverlayComponent } from './nav-overlay/nav-overlay.component';
+
+export const appImports = [
+
+	RouterOutlet,
+
+	HeaderComponent,
+	NavComponent,
+	NavOverlayComponent
+
+]
 
 export const appConfig: ApplicationConfig = {
 
