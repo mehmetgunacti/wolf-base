e21a0975176656680fb8360e1f8deaaaf988e71c
Mehmet Gunacti <mehmet.gunacti@gmail.com>
2024-10-12 02:07:27 +0300

    adjust vscode settings

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,28 @@
-import {Component} from '@angular/core'
-import {RouterOutlet} from '@angular/router'
+import { Component } from "@angular/core";
+import { RouterOutlet } from '@angular/router';
 
 @Component({
 	selector: 'app-root',
 	standalone: true,
-	imports: [RouterOutlet],
+	imports: [ RouterOutlet ],
 	templateUrl: './app.component.html',
 	styleUrl: './app.component.scss',
 })
 export class AppComponent {
-	title = 'wolf-base'
+
+	title = "wolf-base";
+
+	constructor() {
+
+		const a = this.abc("");
+
+	}
+
+	abc(a: string): string {
+
+		console.log();
+		return '';
+
+	}
+
 }
