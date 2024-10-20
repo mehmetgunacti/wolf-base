43a39cab36ffc8032e56006198f117ed910c3247
Mehmet Gunacti <mehmet.gunacti@gmail.com>
2024-10-21 02:06:10 +0300

    remove dexie implementation

--- src/services/index.ts
+++ src/services/index.ts
@@ -1,6 +1,5 @@
 export * from './bookmark-sync.service';
 export * from './clipboard.service';
-export * from './dexie';
 export * from './dom.service';
 export * from './error.handler';
 export * from './firestore';
