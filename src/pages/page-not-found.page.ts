2c4a1c5a020a7812a774891fd953d108877f8c78
Mehmet Gunacti <mehmet.gunacti@gmail.com>
2024-10-30 18:53:47 +0300

    style page not found component

--- src/pages/page-not-found.page.ts
+++ src/pages/page-not-found.page.ts
@@ -1,13 +1,33 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
+import { GlyphDirective } from '@directives';
 import { BaseComponent } from '@libComponents';
 
 @Component({
 	selector: 'page-not-found-page',
 	standalone: true,
-	imports: [],
+	imports: [ GlyphDirective, RouterLink ],
 	template: `
-		<p>page-not-found works!</p>
-	`,
-	host: { 'class': 'page' }
+		<section class="comp m-auto p-16 flex items-center justify-center">
+			<div class="flex flex-col items-center max-w-sm mx-auto text-center">
+				<p class="flex items-center gap-3">
+					<span class="p-3 rounded-full bg-info">
+						<svg wGlyph="info" class="text-4xl"></svg>
+					</span>
+					<span class="text-2xl font-bold">Page not found</span>
+				</p>
+				<p class="mt-4 text-gray-500 dark:text-gray-400">The page you are looking for does not exist.</p>
+				<div class="flex items-center w-full mt-6 gap-x-3 shrink-0 sm:w-auto">
+
+					<button class="btn btn-ghost" routerLink="/">
+						<svg wGlyph="home" class="text-[1.3em]"></svg>
+						Home
+					</button>
+
+				</div>
+			</div>
+		</section>
+    `,
+	host: { 'class': 'page h-full' }
 })
 export class PageNotFoundPage extends BaseComponent { }
