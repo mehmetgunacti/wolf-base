c0b2f4526e710a323c0afdb41dcdeed401bd1f70
Mehmet Gunacti <mehmet.gunacti@gmail.com>
2024-11-15 06:35:06 +0300

    increase bookmark gap

--- src/version.ts
+++ src/version.ts
@@ -1,6 +1,6 @@
 // this file is automatically created
 export const buildInfo = {
-	version: '0.2.20',
-	builtTime: '2024-11-14T06:56:01.573Z',
-	builtNumber: '1536'
+	version: '0.2.21',
+	builtTime: '2024-11-15T03:35:24.024Z',
+	builtNumber: '1540'
 };
