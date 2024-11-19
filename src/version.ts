c216e9b8b4b307c5050e43ff4d6f33bb5f1d1d78
Mehmet Gunacti <mehmet.gunacti@gmail.com>
2024-11-19 17:20:15 +0300

    add button variants

--- src/version.ts
+++ src/version.ts
@@ -1,6 +1,6 @@
 // this file is automatically created
 export const buildInfo = {
-	version: '0.2.24',
-	builtTime: '2024-11-19T05:14:50.709Z',
-	builtNumber: '1557'
+	version: '0.2.25',
+	builtTime: '2024-11-19T14:23:04.643Z',
+	builtNumber: '1560'
 };
