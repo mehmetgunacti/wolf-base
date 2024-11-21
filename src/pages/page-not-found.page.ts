c14768136f0c5afe878bca6b922d334d220cd694
Mehmet Gunacti <mehmet.gunacti@gmail.com>
2024-11-21 07:15:25 +0300

    page not found component styling

--- src/pages/page-not-found.page.ts
+++ src/pages/page-not-found.page.ts
@@ -10,14 +10,14 @@ import { BaseComponent } from '@libComponents/base.component';
 	template: `
 		<section class="comp m-auto p-16 flex items-center justify-center">
 			<div class="flex flex-col items-center max-w-sm mx-auto text-center">
-				<p class="flex items-center gap-3">
+				<p class="flex items-center gap-3 flex-col sm:flex-row">
 					<span class="p-3 rounded-full bg-info">
 						<svg wGlyph="info" class="text-4xl"></svg>
 					</span>
 					<span class="text-2xl font-bold">Page not found</span>
 				</p>
 				<p class="mt-4 text-gray-500 dark:text-gray-400">The page you are looking for does not exist.</p>
-				<div class="flex items-center w-full mt-6 gap-x-3 shrink-0 sm:w-auto">
+				<div class="flex items-center w-full mt-6 gap-x-3 shrink-0 justify-center">
 
 					<button class="btn btn-ghost" routerLink="/">
 						<svg wGlyph="home" class="text-[1.3em]"></svg>
