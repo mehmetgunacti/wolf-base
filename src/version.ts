a6eb95483b166fc82d82da4f04f4901d1750ad4a
Mehmet Gunacti <mehmet.gunacti@gmail.com>
2024-11-21 07:34:09 +0300

    no entities available message styling

--- src/version.ts
+++ src/version.ts
@@ -1,6 +1,6 @@
 // this file is automatically created
 export const buildInfo = {
-	version: '0.2.26',
-	builtTime: '2024-11-21T03:27:31.029Z',
-	builtNumber: '1566'
+	version: '0.2.27',
+	builtTime: '2024-11-21T04:34:23.016Z',
+	builtNumber: '1571'
 };
