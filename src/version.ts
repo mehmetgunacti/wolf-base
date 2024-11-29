7e5a89894a3cb8297ee6d103cde7535600a6511a
Mehmet Gunacti <mehmet.gunacti@gmail.com>
2024-11-29 06:56:07 +0300

    adjust accent text color value

--- src/version.ts
+++ src/version.ts
@@ -1,6 +1,6 @@
 // this file is automatically created
 export const buildInfo = {
-	version: '0.2.46',
-	builtTime: '2024-11-29T03:40:00.556Z',
-	builtNumber: '1621'
+	version: '0.2.47',
+	builtTime: '2024-11-29T03:58:55.825Z',
+	builtNumber: '1624'
 };
