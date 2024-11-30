5219ac377736dbad7dbca92fd7b66564ad752f9d
Mehmet Gunacti <mehmet.gunacti@gmail.com>
2024-11-30 12:24:00 +0300

    remove unused code

--- src/pages/project-edit-form.page.ts
+++ src/pages/project-edit-form.page.ts
@@ -1,6 +1,5 @@
 import { Component } from '@angular/core';
 import { ProjectEditFormContainer } from '@containers/project-edit-form.container';
-import { WordEditFormContainer } from '@containers/word-edit-form.container';
 import { BaseComponent } from '@libComponents/base.component';
 
 @Component({
