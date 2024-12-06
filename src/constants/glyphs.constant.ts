ba488756e78c9eb73282c84da646adb4f7c38026
Mehmet Gunacti <mehmet.gunacti@gmail.com>
2024-12-06 23:21:54 +0300

    add details glyph

--- src/constants/glyphs.constant.ts
+++ src/constants/glyphs.constant.ts
@@ -36,6 +36,7 @@ export type GlyphName =
 	| 'database'
 	| 'data_export'
 	| 'delete'
+	| 'details'
 	| 'description'
 	| 'dictionary'
 	| 'disk'
@@ -356,6 +357,14 @@ export const Glyphs: Record<GlyphName, GlyphData> = {
 		width: '19.2',
 		height: '24',
 		viewBox: '0 -960 768 960'
+
+	},
+	details: {
+
+		width: '16',
+		height: '16',
+		viewBox: '0 0 24 24',
+		d: [ 'M4.25 4A2.25 2.25 0 0 0 2 6.25v2.5A2.25 2.25 0 0 0 4.25 11h2.5A2.25 2.25 0 0 0 9 8.75v-2.5A2.25 2.25 0 0 0 6.75 4zm7 1a.75.75 0 0 0 0 1.5h10a.75.75 0 0 0 0-1.5zm0 3a.75.75 0 0 0 0 1.5h7a.75.75 0 0 0 0-1.5zm-7 5A2.25 2.25 0 0 0 2 15.25v2.5A2.25 2.25 0 0 0 4.25 20h2.5A2.25 2.25 0 0 0 9 17.75v-2.5A2.25 2.25 0 0 0 6.75 13zm7 1a.75.75 0 0 0 0 1.5h10a.75.75 0 0 0 0-1.5zm0 3a.75.75 0 0 0 0 1.5h7a.75.75 0 0 0 0-1.5z' ]
 
 	},
 	dictionary: {
