2283e66ce908b86803388ad27e46f7950645b83f
Mehmet Gunacti <mehmet.gunacti@gmail.com>
2024-12-09 02:10:45 +0300

    add comp to cloud tasks

--- src/version.ts
+++ src/version.ts
@@ -1,6 +1,6 @@
 // this file is automatically created
 export const buildInfo = {
-	version: '0.2.68',
-	builtTime: '2024-12-08T23:08:13.108Z',
-	builtNumber: '1682'
+	version: '0.2.69',
+	builtTime: '2024-12-08T23:10:56.082Z',
+	builtNumber: '1683'
 };
