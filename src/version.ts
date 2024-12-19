5e67598bed522820cafbc83c91497de9bbd29c3e
Mehmet Gunacti <mehmet.gunacti@gmail.com>
2024-12-19 09:09:58 +0300

    fix choice viewer styling

--- src/version.ts
+++ src/version.ts
@@ -1,6 +1,6 @@
 // this file is automatically created
 export const buildInfo = {
-	version: '0.2.72',
-	builtTime: '2024-12-19T04:59:53.580Z',
-	builtNumber: '1687'
+	version: '0.2.73',
+	builtTime: '2024-12-19T06:10:06.808Z',
+	builtNumber: '1688'
 };
