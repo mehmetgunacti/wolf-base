ab5c0fa92e84797f32dbb1b704c1e82c2880cda4
Mehmet Gunacti <mehmet.gunacti@gmail.com>
2025-01-04 20:41:25 +0300

    fix bookmarks grid on mobile

--- src/version.ts
+++ src/version.ts
@@ -1,6 +1,6 @@
 // this file is automatically created
 export const buildInfo = {
-	version: '0.2.83',
-	builtTime: '2025-01-04T17:28:29.254Z',
-	builtNumber: '1709'
+	version: '0.2.84',
+	builtTime: '2025-01-04T17:41:31.261Z',
+	builtNumber: '1710'
 };
