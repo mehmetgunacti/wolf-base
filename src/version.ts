fa92bd5b999676168c6b07ec4c9f30d9f2ccff4a
Mehmet Gunacti <mehmet.gunacti@gmail.com>
2025-01-17 16:44:24 +0300

    fix button label

--- src/version.ts
+++ src/version.ts
@@ -1,6 +1,6 @@
 // this file is automatically created
 export const buildInfo = {
-	version: '0.2.87',
-	builtTime: '2025-01-05T05:51:39.098Z',
-	builtNumber: '1713'
+	version: '0.2.88',
+	builtTime: '2025-01-17T13:44:34.016Z',
+	builtNumber: '1716'
 };
