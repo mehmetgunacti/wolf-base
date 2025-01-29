4d409ebc1fe6b75423b0c492e715dd2e1aa5464f
Mehmet Gunacti <mehmet.gunacti@gmail.com>
2025-01-29 18:18:12 +0300

    add json viewer

--- src/containers/database-entities/database-entities.container.ts
+++ src/containers/database-entities/database-entities.container.ts
@@ -1,14 +1,15 @@
 import { JsonPipe } from '@angular/common';
-import { Component, inject } from '@angular/core';
+import { Component, computed, inject } from '@angular/core';
 import { RouterLink } from '@angular/router';
 import { opacityTrigger } from '@animations/opacity.animation';
 import { GlyphDirective } from '@directives/glyph.directive';
 import { BaseComponent } from '@libComponents/base.component';
+import { MarkdownViewerComponent } from '@libComponents/markdown/markdown-viewer.component';
 import { Store } from '@ngrx/store';
 import { selDatabase_selectedEntity } from '@selectors/database/database.selectors';
 
 @Component({
-	imports: [ JsonPipe, GlyphDirective, RouterLink ],
+	imports: [ JsonPipe, GlyphDirective, RouterLink, MarkdownViewerComponent ],
 	selector: 'app-database-entities-container',
 	templateUrl: './database-entities.container.html',
 	animations: [ opacityTrigger ],
@@ -19,5 +20,11 @@ export class DatabaseEntitiesContainer extends BaseComponent {
 	private store: Store = inject(Store);
 
 	entity = this.store.selectSignal(selDatabase_selectedEntity);
+	entityName = computed(() => {
+
+		const e: any = this.entity();
+		return !!e ? !!Object.hasOwn(e, 'name') ? e[ 'name' ] : null : null;
+
+	});
 
 }
