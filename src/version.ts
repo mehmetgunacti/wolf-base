92cf295145c9f1d8ec4a13b78a5c99575b05c73b
Mehmet Gunacti <mehmet.gunacti@gmail.com>
2025-03-03 19:47:05 +0300

    update angular-eslint

--- src/version.ts
+++ src/version.ts
@@ -1,6 +1,6 @@
 // this file is automatically created
 export const buildInfo = {
-	version: '0.2.93',
-	builtTime: '2025-01-31T21:00:09.978Z',
-	builtNumber: '1727'
+	version: '0.2.94',
+	builtTime: '2025-03-03T16:48:55.248Z',
+	builtNumber: '1732'
 };
