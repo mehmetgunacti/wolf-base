bf54364ca47ba96ebfed35b8179d20b459216c28
Mehmet Gunacti <mehmet.gunacti@gmail.com>
2025-04-09 15:38:51 +0300

    fix alert component text color

--- src/lib/components/alert/alert.component.ts
+++ src/lib/components/alert/alert.component.ts
@@ -10,23 +10,23 @@ import { BaseComponent } from '../base.component';
 	host: {
 		'class': 'flex items-center border rounded-lg min-h-[calc(1.5*var(--widget-height))] shadow-component',
 
-		'[class.text-primary]': 'severity() === "primary"',
+		'[class.text-primary-text]': 'severity() === "primary"',
 		'[class.bg-primary]': 'severity() === "primary"',
 		'[class.border-primary]': 'severity() === "primary"',
 
-		'[class.text-success]': 'severity() === "success"',
+		'[class.text-success-text]': 'severity() === "success"',
 		'[class.bg-success]': 'severity() === "success"',
 		'[class.border-success]': 'severity() === "success"',
 
-		'[class.text-info]': 'severity() === "info"',
+		'[class.text-info-text]': 'severity() === "info"',
 		'[class.bg-info]': 'severity() === "info"',
 		'[class.border-info]': 'severity() === "info"',
 
-		'[class.text-warn]': 'severity() === "warn"',
+		'[class.text-warn-text]': 'severity() === "warn"',
 		'[class.bg-warn]': 'severity() === "warn"',
 		'[class.border-warn]': 'severity() === "warn"',
 
-		'[class.text-error]': 'severity() === "error"',
+		'[class.text-error-text]': 'severity() === "error"',
 		'[class.bg-error]': 'severity() === "error"',
 		'[class.border-error]': 'severity() === "error"',
 	}
