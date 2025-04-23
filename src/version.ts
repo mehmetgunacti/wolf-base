41797b88daed8112f4485aeb16a638c94f08a33c
Mehmet Gunacti <mehmet.gunacti@gmail.com>
2025-04-23 14:12:23 +0300

    add content available flag to note content form

--- src/version.ts
+++ src/version.ts
@@ -1,6 +1,6 @@
 // this file is automatically created
 export const buildInfo = {
-	version: '0.2.102',
-	builtTime: '2025-04-10T18:31:38.369Z',
-	builtNumber: '1766'
+	version: '0.2.103',
+	builtTime: '2025-04-23T11:12:29.700Z',
+	builtNumber: '1767'
 };
